012f25e92d5ce4e8c8a7baff4b6feb43ebc42c1f
rruiliu1005 <42427868+rruiliu1005@users.noreply.github.com>
2024-05-05 16:31:17 +1000

    Modified model specification

--- old_code.R
+++ old_code.R
@@ -65,3 +65,62 @@
 # average_matrix_A <- average_matrix_A / length(sampled_posterior_A)
 # print(round(average_matrix_A,3))
 ```
+data = random_walk_sample
+N = ncol(data)
+T = nrow(data)
+X = matrix(1,T,1)
+S = S1 + S2
+for (i in 1:p){
+  X     = cbind(X,data[p+1:T-i,])
+}
+
+sigma_h_squared = rinvgamma(n = 1, alpha = vh0_prior, beta = sh0_prior)
+rho = rtnorm(n = 1, mean = rho_prior, sqrt(Vrho_prior), a = -1, b = 1)
+u_h = rnorm(n = T, mean = 0, sd = sqrt(sigma_h_squared))
+h = array(dim = T)
+u = array(dim = c(N,1,T))
+h [1] = rho * 0 + u_h[1]
+psi = rtnorm(n = 1, mean = psi_prior, sd = sqrt(Vpsi_prior), a = -1, b = 1)
+epsilon = array(dim = c(N,1,T))
+Omega = array(0, dim = c(T,T))
+
+for (t in 2:T){
+  h[t] = rho*h[t-1] + u_h[t]
+} 
+
+Sigma_prior = rWishart(n = 1, df = nu_prior, Sigma = S_prior)[,,1]
+Sigma_prior = solve(Sigma_prior)
+
+for (t in 1:T){
+  u_var = exp(h[t])*Sigma_prior
+  u[,,t] = mvrnorm(n = 1, mu = rep(0,N), Sigma = u_var)
+  if (t == 1){
+    epsilon[,,t] = u[,,t]
+    Omega[t,t] = (1+psi^2)*exp(h[t])
+  }
+  else{
+    epsilon[,,t] = u[,,t] + psi*u[,,t-1]
+    Omega[t,t] = psi^2*exp(h[t-1]) + exp(h[t])
+    Omega[t-1,t] = psi * exp(h[t-1])
+    Omega[t,t-1] = psi * exp(h[t-1]) 
+  }
+}
+
+V_bar = solve(t(X)%*%solve(Omega)%*%X + solve(V_prior))
+A_bar = V_bar%*%(t(X)%*%solve(Omega)%*%data + solve(V_prior)%*%A_prior)
+nu_bar = T + nu_prior
+#S_bar  = S_prior + t(data)%*%data + t(A_prior)%*%diag(1/diag(V_prior))%*%A_prior - t(A_bar)%*%solve(V_bar)%*%A_bar
+S_bar = S_prior +  t(data)%*%solve(Omega)%*%data + t(A_prior)%*%A_prior - t(A_bar)%*%solve(V_bar)%*%A_bar
+S_bar_inv = solve(S_bar)
+#Sigma_posterior = rWishart(S, df=nu_bar, Sigma=S_bar_inv)
+Sigma_posterior = rWishart(S, df=nu_bar, Sigma=S_bar_inv)
+Sigma_posterior = apply(Sigma_posterior,3,solve)
+Sigma_posterior  = array(Sigma_posterior,c(N,N,S))
+A_posterior = array(rnorm(prod(c(dim(A_bar),S))),c(dim(A_bar),S))
+L = t(chol(V_bar))
+
+for (s in 1:S){
+  A_posterior[,,s]= A_bar + L%*%A_posterior[,,s]%*%chol(Sigma_posterior[,,s])
+}
+A_posterior = A_posterior[,,(S1+1):(S1+S2)]
+Sigma_posterior = Sigma_posterior[,,(S1+1):(S1+S2)]

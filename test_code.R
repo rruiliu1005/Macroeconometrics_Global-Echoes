f5ac9638231bfec2854d941c93dac080ff8ec19e
rruiliu1005 <42427868+rruiliu1005@users.noreply.github.com>
2024-05-09 14:20:08 +1000

    Corrected typos

--- test_code.R
+++ test_code.R
@@ -39,6 +39,9 @@ S_prior     = diag(1, N)
 nu_prior    = ncol(random_walk_sample)+1
 S1 = 100
 S2 = 200
+tau = 0.2
+psi_prior = 0 
+Vpsi_prior = 1
 
 # posterior_samples = BVAR_MA_fit(data = random_walk_sample, p = p, S1= S1, S2 = S2, kappa1 = kappa1, kappa2 = kappa2, A_prior = A_prior, V_prior = V_prior, S_prior = S_prior, nu_prior = nu_prior)
 # posterior_samples_A = posterior_samples[[1]]
@@ -87,12 +90,14 @@ S2 = 200
   Omega_posterior = array(dim = c(Ty, Ty, S+1))
   Omega_posterior[,,1] = H_psi %*% O_psi %*% t(H_psi)
   
+  accept = 0
   
   pb <- progress_bar$new(total = S)
-    
+  
   for (s in 1:S){
     pb$tick()
-    chol_Omega = chol(Omega_posterior[,,1]) #########change 
+
+    chol_Omega = chol(Omega_posterior[,,s]) #########change 
     chol_Omega_inv = solve(chol_Omega)
     #V_bar = solve(t(X)%*%X + solve(V_prior))
     V_bar = solve(t(chol_Omega_inv%*%X)%*%(chol_Omega_inv%*%X) + solve(V_prior))
@@ -104,6 +109,42 @@ S2 = 200
     S_bar_inv = solve(S_bar)
     Sigma_posterior[,,s] = solve(rWishart(1, df=nu_bar, Sigma=S_bar_inv)[,,1])
     A_posterior[,,s]= A_bar + L%*%A_posterior[,,s]%*%chol(Sigma_posterior[,,1]) ############change 
+    ###############MH Step    ##################################
+    psi_candidate = rnorm(n = 1, mean = psi, sd = tau)
+    if(abs(psi_candidate) > 1){
+      alpha = 0
+    }
+    else{
+      H_psi = matrix(0, nrow = Ty, ncol = Ty)
+      O_psi = diag(rep(1, Ty))
+      O_psi[1,1] = 1+psi_candidate^2
+      for (i in 1:Ty){
+        H_psi[i,i] = 1
+        if (i < Ty){
+          H_psi[i+1, i] = psi_candidate
+        }
+      }
+      
+      
+      Omega_draw = H_psi %*% O_psi %*% t(H_psi)
+      chol_Omega_draw = chol(Omega_draw)
+      chol_Omega_draw_inv = solve(chol_Omega_draw)
+      p_y_numerator = ((1 + psi_candidate^2)/( 1 + psi^2))^(-N/2)*exp(-1/2*tr(solve(Sigma_posterior[,,s])%*%t(chol_Omega_inv%*%(Y-X%*%A_posterior[,,s]))%*%(chol_Omega_inv%*%(Y-X%*%A_posterior[,,s])))
+                                                                      +1/2*tr(solve(Sigma_posterior[,,s])%*%t(chol_Omega_draw_inv%*%(Y-X%*%A_posterior[,,s]))%*%(chol_Omega_inv%*%(Y-X%*%A_posterior[,,s]))) )
+      numerator = p_y_numerator * dtruncnorm(x=psi_candidate, mean=psi_prior, sd = Vpsi_prior, a = -1, b = 1) *dnorm(x = psi, mean = psi_candidate, sd = tau)
+      denominator =  dtruncnorm(x=psi, mean=psi_prior, sd = Vpsi_prior, a = -1, b = 1) *dnorm(x = psi_candidate, mean = psi, sd = tau)
+      alpha = min(1, numerator/denominator)
+    }
+    u = runif(1, min = 0, max = 1)
+      #print(numerator/denominator)
+    if (u <= alpha){
+      accept = accept + 1
+      psi = psi_candidate
+      Omega_posterior[,,s+1] = Omega_draw
+      }
+    else{
+      Omega_posterior[,,s+1] = Omega_posterior[,,s]}
+    ##################################################    #####
   }
   A_posterior = A_posterior[,,(S1+1):(S1+S2)]
   Sigma_posterior = Sigma_posterior[,,(S1+1):(S1+S2)]
@@ -117,9 +158,13 @@ S2 = 200
 
   print('Posterior standard deviation of autoregressive parameter:')
   round(apply(A_posterior,1:2,sd),3)
-  print('Posterior standard deviation of covariance matrix:')
+  print('Posterior standard deviation of row specific covariance matrix:')
   round(apply(Sigma_posterior,1:2,sd),3)
+  
   print('Posterior mean of autoregressive parameter:')
   round(apply(A_posterior,1:2,mean),3)
-  print('Posterior mean of covariance matrix:')
+  print('Posterior mean of column specific covariance matrix:')
+  Omega_posterior_mean = round(apply(Omega_posterior,1:2,sd),3)
+  Omega_posterior_mean[1:10, 1:10]
+  print('Posterior mean of row specific covariance matrix:')
   round(apply(Sigma_posterior,1:2,mean),3)

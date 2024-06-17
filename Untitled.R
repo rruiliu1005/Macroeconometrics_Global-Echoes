6401aa4917a63ddb48dfb03507242a11cc9a7abf
rruiliu1005 <42427868+rruiliu1005@users.noreply.github.com>
2024-06-17 22:30:52 +0800

    Updated SDDR

---
+++ Untitled.R
@@ -0,0 +1,6 @@
+## 4.3  Large BVAR model with Common Stochastic Volatility
+### 4.3.1 Model Building Code and Validation 
+
+## 4.4  Large BVAR model with MA(1) Gaussian Innovations and Common Stochastic Volatility
+### 4.4.1 Model Building Code and Validation 
+### 4.4.2 Empirical Result
